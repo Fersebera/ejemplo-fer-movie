4f6f8f625f415725a2219238392a0c91044fbff7
fersebera <fersebera@gmail.com>
2026-02-24 00:03:42 -0300

    CommitMaquetado

--- fer-movie-mvc/Controllers/HomeController.cs
+++ fer-movie-mvc/Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 using System.Diagnostics;
+using fer_movie_mvc.Data;
 using fer_movie_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace fer_movie_mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MovieDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MovieDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var peliculas = await _context.Peliculas.ToListAsync();
+
+            return View(peliculas);
         }
 
         public IActionResult Privacy()
